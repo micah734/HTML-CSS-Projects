8355f31b15895669ee91cb6608ff414c6733e7e3
Micah Gooden <micah734@me.com>
2021-12-15 17:16:27 -0500

    updated to a function lightbox

---
+++ Basic_HTML_and_CSS/One-Page Website/JS/one_page_website.js
@@ -0,0 +1,36 @@
+//Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+//close Modal
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+//Thumbnail Image control
+function plusSlides(n) {
+    showSlides(slideIndex = +n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("pic");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; 0 < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
